9383a38b39f08d78c7339d9aa19b6212454609a8
nikasenia <niks@uma.es>
2024-05-12 13:05:56 +0200

    This is the first commit

---
+++ travis-ci-example/src/test/java/com/calidad/travisciexample/TravisCiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calidad.travisciexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
